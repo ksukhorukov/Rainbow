9f7cfb426d76f33d9929175464d685838f23bc97
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-15 14:42:58 +0800

    mess with args

--- generate_table.rb
+++ generate_table.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'digest'
+require 'byebug'
 require 'pp'
 
 class TableGenerator 
-  attr_reader :passwords, :alg, :out
+  attr_reader :passwords, :alg, :input, :out, :crypto_algs_available
 
   def initialize
     usage
     parse_args
   end 
+
+  def close_output_file
+    output_file.close
+  end
+
+private 
 
   def usage
     if ARGV.count < 2
@@ -20,10 +27,39 @@ class TableGenerator
   end
 
   def parse_args
-    pp ARGV
+    @input ||= ARGV[1]
+    @alg ||= ARGV[2]
+    @out ||= ARGV[3]
+    
+    raise 'unsupported crypto alg' unless available_alg? alg 
+
+    puts "#{input} #{out} #{alg}"
   end
+
+  def crypto_algs
+    @crypto_algs_available ||= %w[md5 sha256 sha512]
+  end 
+
+  def available_alg? alg 
+    crypto_algs_available.include? alg&.to_s
+  end 
+
+  def passwords 
+    @passwords ||= File.readlines(input)
+  end 
+
+  def output_file 
+    @output_file ||= File.open(out, 'w')
+  end 
 end 
 
-TableGenerator.new
+
+begin
+  instance = TableGenerator.new
+  instance.close_output_file
+rescue
+  instance.close_output_file
+end 
+
 
 
